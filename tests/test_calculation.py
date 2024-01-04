a8a6d9e90a36bc3e6d4d073660bbd87e89257fb9
unknown <mhaseeb1604@gmail.com>
2024-01-04 17:23:37 +0500

    Calculation file added to try basic testing

--- tests/test_calculation.py
+++ tests/test_calculation.py
@@ -1,5 +1,31 @@
-def test_add():
-    print("Test Function")
-    assert True
+import pytest
 
-test_add()
+def add(x, y):
+    return x+y
+
+def subtract(x, y):
+    return x-y
+
+@pytest.mark.parametrize(
+    "num1, num2, expected",
+    [
+        (1, 2, 3),
+        (5, 9, 14),
+        (8, 5, 13)
+    ]
+)
+
+
+def test_add(num1, num2, expected):
+    assert add(num1, num2) == expected
+
+@pytest.mark.parametrize(
+    "num1, num2, expected",
+    [
+        (1, 2, -1),
+        (9, 5, 4),
+        (8, 5, 3)
+    ]
+)
+def test_subtract(num1, num2, expected):
+    assert subtract(num1, num2) == expected
