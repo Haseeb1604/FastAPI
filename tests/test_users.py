87664408bd278dfcefd1c1ea718c1d2de8a9f897
unknown <mhaseeb1604@gmail.com>
2024-01-04 23:22:05 +0500

    test user added

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,33 @@
+import pytest
 from fastapi.testclient import TestClient
 from app.main import app
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from app import schema
+from app.config import settings
+from app.database import get_db
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+app.dependency_overrides[get_db] = override_get_db()
 
 client = TestClient(app)
 
@@ -9,10 +37,17 @@ def test_root():
     assert res.json().get("data") == "Hello World"
     assert res.status_code == 200
 
-def test_user():
+@pytest.mark.parametrize(
+    "email, password",
+    [
+        ("mhaseeb@gmail.com", "1234")
+    ]
+    )
+def test_user(email, password):
     res = client.post(
         "/users/", 
-        json={"email": "haseeb@gmail.com", "password": "1234"}
+        json={"email": email, "password": password}
             )
     print(res.json())
+    assert res.json().get("email") == email
     assert res.status_code == 201
