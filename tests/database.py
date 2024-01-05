a2318df1b93b2e6b6210d7123031af4be19da8a1
unknown <mhaseeb1604@gmail.com>
2024-01-05 11:32:17 +0500

    Test Dataset setting for testing new file dataset.py for handling dataset operations for test

---
+++ tests/database.py
@@ -0,0 +1,43 @@
+import pytest
+from fastapi.testclient import TestClient
+from app.main import app
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from app.config import settings
+
+from app.Database import get_db
+from app.Database import Base
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}_test"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+@pytest.fixture
+def session():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@pytest.fixture
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+
+    yield TestClient(app)
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,53 +1,26 @@
 import pytest
-from fastapi.testclient import TestClient
-from app.main import app
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
-
 from app import schema
-from app.config import settings
-from app.database import get_db
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-app.dependency_overrides[get_db] = override_get_db()
-
-client = TestClient(app)
-
-def test_root():
+def test_root(client):
     res = client.get("/")
     print(res.json())
     assert res.json().get("data") == "Hello World"
     assert res.status_code == 200
 
-@pytest.mark.parametrize(
-    "email, password",
+
+
+@pytest.mark.parametrize("email, password", 
     [
-        ("mhaseeb@gmail.com", "1234")
+        ("mhaseeb1@gmail.com", "1234"),
     ]
-    )
-def test_user(email, password):
+)
+def test_user(client, email, password):
     res = client.post(
         "/users/", 
         json={"email": email, "password": password}
             )
-    print(res.json())
-    assert res.json().get("email") == email
+    new_user = schema.UserOut(**res.json())
+    print(new_user.email)
+    assert new_user.email == email
     assert res.status_code == 201
